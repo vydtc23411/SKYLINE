769546637a117a8c44990937fb60d7c7cff81b52
Nguyễn Ngọc Tường Vy <vynnt23411@st.uel.edu.vn>
2025-11-15 16:46:44 +0700

    chỉnh trang intro

--- src/app/user/intro/intro.ts
+++ src/app/user/intro/intro.ts
@@ -14,22 +14,37 @@ export class Intro {
   teamMembers = [
     { name: 'Trịnh Thị Thùy Trang', image: 'assets/img/intro/thuytrang.png' },
     { name: 'Phạm Thị Hoài Thương', image: 'assets/img/intro/hoaithuong.png' },
-    { name: 'Đào Thị Cẩm Vy', image: 'assets/img/intro/camvy.png', featured: true },
+    { name: 'Đào Thị Cẩm Vy', image: 'assets/img/intro/camvy.png' },
     { name: 'Trần Thị Thiên Thảo', image: 'assets/img/intro/thienthao.png' },
     { name: 'Nguyễn Ngọc Tường Vy', image: 'assets/img/intro/tuongvy.png' }
   ];
 
-  currentSlide = 0;
+  featuredIndex = 2; // Member ở giữa (index 2)
 
   nextSlide() {
-    if (this.currentSlide < this.teamMembers.length - 3) {
-      this.currentSlide++;
+    if (this.featuredIndex < this.teamMembers.length - 1) {
+      this.featuredIndex++;
     }
   }
 
   prevSlide() {
-    if (this.currentSlide > 0) {
-      this.currentSlide--;
+    if (this.featuredIndex > 0) {
+      this.featuredIndex--;
     }
+  }
+
+  isFeatured(index: number): boolean {
+    return index === this.featuredIndex;
+  }
+
+  getVisibleMembers() {
+    // Hiển thị 5 members: 2 bên trái, 1 giữa (featured), 2 bên phải
+    const start = Math.max(0, this.featuredIndex - 2);
+    const end = Math.min(this.teamMembers.length, this.featuredIndex + 3);
+    return this.teamMembers.slice(start, end);
+  }
+
+  getMemberIndex(member: any): number {
+    return this.teamMembers.indexOf(member);
   }
 }
